b0e6ae5edbb053830305bc307647ff4c44afc84c
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-10-13 05:59:39 +0300

    Added  Contact section :rocket: :relaxed:

--- src/contact.js
+++ src/contact.js
@@ -39,48 +39,6 @@ const Contact = props => {
             </Box>
 
         </Grid>
-
-
-        //         <footer class="bg-dark text-white mt-4">
-        //           <div class="row">
-        //             <div class="col-md-4 mt-5 p-5">
-        //   <h1>Contact Info</h1>
-        //   <h4>info@AppsOnPoint.com</h4>
-        //   <h4>0733 330 395</h4>
-
-        //   <h1 class="mt-5">Address</h1>
-        //   <h4>P.O. BOX 1302</h4>
-        //   <h4>Nairobi, Kenya</h4>
-
-        //   <h1 class="mt-5">Location</h1>
-        //   <h4>Hurlingham, Nairobi</h4>
-
-        //             </div>
-        //             <div class="col-md-4 mt-5 p-5">
-        //               <h1 class="mt-5">Navigation</h1>
-        //               <h4>
-        //               <a class="text-white js-scroll-trigger" href="#page-top">Home</a>
-
-        //               </h4>
-        //               <h4>
-        //                 <a class="text-white js-scroll-trigger" href="#about">About</a>
-        //               </h4>
-        //               <h4>
-        //                 <a class="text-white js-scroll-trigger" href="#services">Services</a>
-        //               </h4>
-        //               <h4>
-        //                 <a class="text-white js-scroll-trigger" href="#team">Our Team</a>
-        //               </h4>
-        //               <h4>
-        //                 <a class="text-white js-scroll-trigger" href="#contact">Contact</a>
-        //               </h4>
-
-        //             </div>
-        //             <div class="col-md-4 mt-5 p-5">
-        //               <img src="/Annotation 2020-09-21 234646_1.png" alt="" />
-        //             </div>
-        //           </div>
-        //         </footer>
     );
 }
 
