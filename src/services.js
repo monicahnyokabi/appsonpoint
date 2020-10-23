4670588be644bc2c97ef4e5528d17088252fb7e3
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-10-23 17:05:57 +0300

    added changes

--- src/services.js
+++ src/services.js
@@ -1,18 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import { Grid, Box, Text, Image, Stack, SimpleGrid } from "@chakra-ui/core";
- 
-=======
-import Mobile from "./components/service/mobile";
-import Responsive from "./components/service/responsive";
-import Security from "./components/service/security";
-import Scalability from "./components/service/scalability";
-import System from "./components/service/system";
-import Consultancy from "./components/service/consultancy";
 
-import { Grid, Box, Text } from "@chakra-ui/core";
-
->>>>>>> 6258e1b189d9c6d8dfd42de773aacd6400ab2dce
 const Services = props => {
    return (
        <Grid templateRows="repeat(2, 1fr)" gap={6} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} mr={{ md: 6 }} >
@@ -22,7 +10,7 @@ const Services = props => {
            <Grid templateColumns="repeat(3,1fr)" gap={6}>
            const mobileApplication = props => {
     return (
-<<<<<<< HEAD
+
         <Box w="100%" h="10">
             <Stack isInline>
                 <Image
@@ -115,41 +103,7 @@ const Services = props => {
                         </Stack>
                     </Box>
                         
-=======
-        <Grid templateRows="repeat(2, 2fr)" gap={6} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} mr={{ md: 6 }} >
 
-            <Box borderRadius="50px 0px 50px 0px"
-                background="#3658fe"
-                h='80px'
-                w='250px'
-                padding='15px'
-                boxShadow='lg'
-                mt={{ base: 4, md: 16 }}
-            >
-                <Text as='h3'
-                    color='white'
-                    textAlign='center'
-                    padding='15px'>
-                    Services
-                </Text>
-                
-            </Box>
-
-            <Grid templateColumns="repeat(3,1fr)" gap={6} ml={{ md: 6 }} mr={{ md: 6 }}>
-                <Mobile />
-                <Responsive />
-                <Security />
-            </Grid>
-
-            <Grid templateColumns="repeat(3,1fr)" gap={6}>
-                <Scalability/>
-                <System/>
-                <Consultancy/>
-            </Grid>
-        </Grid>
-    );
-};
->>>>>>> 6258e1b189d9c6d8dfd42de773aacd6400ab2dce
 
                         const ItConsultancy = props => {
                             return (
--- src/team.js
+++ src/team.js
@@ -1,5 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
+
 import { Grid, Box, Text, Image, Stack, SimpleGrid } from "@chakra-ui/core";
  
 const Team = props => {
@@ -26,43 +26,6 @@ const Team = props => {
              
             const Team = [{icon:"/avataaars.png", title:'CEO', name:"John Doe"} {icon="/avataaars.png title='COO' name="Mark Kinyanjui"}{icon="/avataaars.png title='CTO' name="Peter Owoka"} ]
    );
-=======
-import { Grid, Box, Text, Image } from "@chakra-ui/core";
-import CEO from "./components/team/ceo";
-import CTO from "./components/team/cto";
-import COO from "./components/team/coo";
-import Hiring from "./components/team/hiring";
-
-const Team = props => {
-    return (
-        <Grid templateRows="repeat(1, 1fr)" gap={6} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} mr={{ md: 6 }} >
-
-            <Box borderRadius="50px 0px 50px 0px"
-                background="#3658fe"
-                h='80px'
-                w='250px'
-                padding='15px'
-                boxShadow='lg'
-                mt={{ base: 4, md: 16 }}
-            >
-
-                <Text as='h3'
-                    color='white'
-                    textAlign='center'
-                    padding='15px'>
-                    Team
-                </Text>
-            </Box>
-
-            <Grid templateColumns="repeat(4,8fr)" gap={6} mt={{ base: 4, md: 4 }} ml={{ md: 6 }} mr={{ md: 6 }}>
-                <CEO />
-                <CTO />
-                <COO />
-                <Hiring />
-            </Grid>
-        </Grid>
-    );
->>>>>>> 6258e1b189d9c6d8dfd42de773aacd6400ab2dce
 }
  
 export default Team;
